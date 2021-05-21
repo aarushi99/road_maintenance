1f0974701ee90db880f36f5d13e4f77be118b7da
rajat77 <rajatseniwal1001@gmail.com>
2021-05-21 14:17:58 +0530

    new navbar

--- frontend/road_maintenance_frontend/src/components/navbar.js
+++ frontend/road_maintenance_frontend/src/components/navbar.js
@@ -2,13 +2,17 @@ import React from "react";
 import "./navbar.css";
 
 function navbar(props) {
-  return (
-    <div className="header">
-      <div className="header-left">Menu</div>
-      <div className="header-middle">Home</div>
-      <div className="header-right">Logout</div>
-    </div>
-  );
+	return (
+		<div className="header">
+			<div className="header-title header-item">
+				MC Road Maintainence System
+			</div>
+			<div className="header-item">About Us</div>
+			<div className="header-item">Contact</div>
+			<div className="header-item">Help</div>
+			<div className="header-item">Logout</div>
+		</div>
+	);
 }
 
 export default navbar;
