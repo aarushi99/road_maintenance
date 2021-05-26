c8aee025b3549bb1f3ff36f16efc4404285c28b5
rajat77 <rajatseniwal1001@gmail.com>
2021-05-26 16:22:13 +0530

    legend in marker detail

--- frontend/road_maintenance_frontend/src/components/MarkerDetail.js
+++ frontend/road_maintenance_frontend/src/components/MarkerDetail.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react";
 import Map from "./map";
 import History from "./History";
 import Dialogbox from "./Dialogbox";
+import RedIcon from "../images/marker icons/redIcon.png";
+import BlueIcon from "../images/marker icons/blueIcon.png";
+import OrangeIcon from "../images/marker icons/orangeIcon.png";
 
 function MarkerDetail(props) {
-  const params = props.match.params;
-  return (
+	const params = props.match.params;
+	return (
 		// <div className="container-main">
 		//   <div className="container-main-map">
 		//     <div className="container-main-map-box">
@@ -21,6 +24,32 @@ function MarkerDetail(props) {
 			<div className="container-main-map">
 				<div className="container-main-menu">
 					<Dialogbox token={props.token}></Dialogbox>
+					<div className="container-main-menu-legend">
+						<div>
+							<img
+								className="bi me-2"
+								style={{ width: "35px", height: "35px", cursor: "pointer" }}
+								src={RedIcon}
+							/>
+							High
+						</div>
+						<div>
+							<img
+								className="bi me-2"
+								style={{ width: "35px", height: "35px", cursor: "pointer" }}
+								src={OrangeIcon}
+							/>
+							Medium
+						</div>
+						<div>
+							<img
+								className="bi me-2"
+								style={{ width: "35px", height: "35px", cursor: "pointer" }}
+								src={BlueIcon}
+							/>
+							Low
+						</div>
+					</div>
 				</div>
 				<div className="container-main-map-box">
 					<Map mId={params.mId} token={props.token}></Map>
